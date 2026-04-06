4c4ee8c484da657051655fe4d5095d7b04b2e289
Eliaukkeyi <1295973684@qq.com>
2026-04-06 19:34:45 +0800

    Pipe Communication

---
+++ pipe_comm/pipe_block_write.c
@@ -0,0 +1,69 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <fcntl.h>
+
+#define WRITE_SIZE 4096
+
+int main() {
+    int fd[2];
+    pid_t pid;
+
+    if (pipe(fd) == -1) {
+        perror("pipe failed");
+        exit(EXIT_FAILURE);
+    }
+
+#ifdef F_GETPIPE_SZ
+    int pipe_size = fcntl(fd[0], F_GETPIPE_SZ);
+    if (pipe_size != -1) {
+        printf("父进程：当前管道默认大小 = %d 字节\n", pipe_size);
+    }
+#endif
+
+    pid = fork();
+    if (pid < 0) {
+        perror("fork failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid == 0) {
+        close(fd[0]);
+
+        char buf[WRITE_SIZE];
+        memset(buf, 'A', sizeof(buf));
+
+        int count = 0;
+        while (1) {
+            count++;
+            printf("子进程：准备进行第 %d 次写入...\n", count);
+            write(fd[1], buf, sizeof(buf));
+            printf("子进程：第 %d 次写入完成，写入 %ld 字节\n", count, sizeof(buf));
+        }
+
+        close(fd[1]);
+        exit(0);
+    } else {
+        close(fd[1]);
+
+        printf("父进程：先睡眠10秒，不读取管道，观察子进程是否会阻塞...\n");
+        sleep(15);
+
+        printf("父进程：开始读取管道数据，释放管道空间...\n");
+
+        char read_buf[WRITE_SIZE];
+        int n;
+        while ((n = read(fd[0], read_buf, sizeof(read_buf))) > 0) {
+            printf("父进程：读取了 %d 字节\n", n);
+            sleep(1);
+        }
+
+        close(fd[0]);
+        wait(NULL);
+    }
+
+    return 0;
+}
--- pipe_comm/pipe_main.c
+++ pipe_comm/pipe_main.c
@@ -5,22 +5,99 @@
  * 功能：父进程创建管道并与3个子进程通信
  * 关键系统调用：pipe(), fork(), read(), write(), close(), wait()
  */
-
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <semaphore.h>
+#include <errno.h>
+
+#define CHILD_NUM 3
+#define MSG_SIZE 128
 
 int main() {
     int fd[2];
+    pid_t pid;
+    char buffer[MSG_SIZE];
+    sem_t *write_sem;
 
-    // TODO: 创建管道
-    // TODO: 创建3个子进程
-    // TODO: 子进程通过管道写入消息
-    // TODO: 父进程等待子进程完成
-    // TODO: 父进程从管道读取消息
-    // TODO: 分析阻塞型读写现象
+    if (pipe(fd) == -1) {
+        perror("pipe failed");
+        exit(EXIT_FAILURE);
+    }
+
+    sem_unlink("/pipe_write_sem");
+    write_sem = sem_open("/pipe_write_sem", O_CREAT, 0644, 1);
+    if (write_sem == SEM_FAILED) {
+        perror("sem_open failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("父进程：管道创建成功。\n");
+
+#ifdef F_GETPIPE_SZ
+    int pipe_size = fcntl(fd[0], F_GETPIPE_SZ);
+    if (pipe_size != -1) {
+        printf("父进程：当前管道默认大小 = %d 字节\n", pipe_size);
+    } else {
+        perror("fcntl failed");
+    }
+#endif
+
+    for (int i = 0; i < CHILD_NUM; i++) {
+        pid = fork();
+
+        if (pid < 0) {
+            perror("fork failed");
+            exit(EXIT_FAILURE);
+        }
+
+        if (pid == 0) {
+            close(fd[0]);
+
+	    sleep(3);
+	    
+            sem_wait(write_sem);
+
+            char msg[MSG_SIZE];
+            snprintf(msg, sizeof(msg),
+                     "来自子进程 %d (pid=%d) 的消息\n", i + 1, getpid());
+
+            printf("子进程 %d：准备写入管道...\n", i + 1);
+            write(fd[1], msg, strlen(msg));
+            printf("子进程 %d：写入完成。\n", i + 1);
+
+            sem_post(write_sem);
+
+            close(fd[1]);
+            exit(0);
+        }
+    }
+
+    close(fd[1]);
+
+    printf("父进程：现在开始读取管道，如果管道为空，read会阻塞...\n");
+
+    int n = read(fd[0], buffer, sizeof(buffer));
+    printf("父进程：read返回，读取到 %d 字节。\n", n);
+
+    if (n > 0) {
+	write(STDOUT_FILENO, "父进程收到：", strlen("父进程收到："));
+	write(STDOUT_FILENO, buffer, n);
+    }
+
+    for (int i = 0; i < CHILD_NUM; i++) {
+	wait(NULL);
+    }
+
+    close(fd[0]);
+
+    sem_close(write_sem);
+    sem_unlink("/pipe_write_sem");
 
     return 0;
 }
